a3c89e738f768a36d332ca040561a30494f8b305
Bintang Pratama <bp070798@gmail.com>
2018-11-20 23:14:50 +0700

    Add Route

--- addrute.php
+++ addrute.php
@@ -1,13 +1,13 @@
 <div class="row">
-  INPUT RUTE ARMADA <?php $idd=$_GET['id'];$sss=mysqli_fetch_array(mysqli_query($db,"SELECT * FROM data_armada where id=$idd" )); echo $sss[nama]; ?>
+  INPUT RUTE ARMADA <?php $idd=$_GET['id'];$sss=mysqli_fetch_array(mysqli_query($db,"SELECT * FROM forwarder_vehicles where id=$idd" )); echo $sss[nama]; ?>
   <form action="function/tambahrute.php?id=<?php echo $idd; ?>" method="POST">
 <table class="col-md-12" id="tabel_barang" >
    <tr id="row1">
    <td><label>Kota Asal</label> </td> <td>:</td>  <td><input type="text" class="form-control1" name="asal[]" placeholder="PILIH KOTA ASAL" list="cityname">
 <datalist id="cityname">
-  <?php  $slctasal=mysqli_query($db,"SELECT * FROM kabupaten"); while ($dtt=mysqli_fetch_array($slctasal)) {
+  <?php  $slctasal=mysqli_query($db,"SELECT * FROM locations where parent_id!=0"); while ($dtt=mysqli_fetch_array($slctasal)) {
     ?>
-  <option value="<?php echo $dtt[nama]; ?>">
+  <option value="<?php echo $dtt[name_id]; ?>">
   <?php } ?>
 </datalist></td>
    <td><label> Kota Tujuan </label></td> <td>:</td>  <td><input type="text" class="form-control1" name="tujuan[]" placeholder="PILIH KOTA TUJUAN" list="cityname"></td>
