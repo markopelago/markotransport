7f46238143f4ea258173c863758457c86d587f3d
hendro <hendro@gmail.com>
2018-11-20 21:50:31 +0700

    Fixing Login

--- function/usersession.php
+++ function/usersession.php
@@ -4,13 +4,14 @@ include 'db.php';
 if ($act=='login') {
 $email=$_POST['form-email'];
 $unenrcrypted=$_POST['form-password'];
+$clientip=$_SERVER['REMOTE_ADDR'];
 $password=base64_encode($unenrcrypted);
-$ceklogin=mysqli_fetch_array(mysqli_query($db,"SELECT * from transporter where email='$email' AND password='$password'"));
+$ceklogin=mysqli_fetch_array(mysqli_query($db,"SELECT * from a_users where email='$email' AND password='$password'"));
 if ($ceklogin['id']==TRUE) {
-$_SESSION['transporter_email']=$ceklogin['email'];
-$_SESSION['nama_transporter']=$ceklogin['nama'];
-$_SESSION['id_transporter']=$ceklogin['id'];
-$updatelogin=mysqli_query($db,"UPDATE transporter set last_login=$date where email=$email");
+$_SESSION['email']=$ceklogin['email'];
+$_SESSION['name']=$ceklogin['name'];
+$_SESSION['id']=$ceklogin['id'];
+$updatelogin=mysqli_query($db,"UPDATE transporter set last_sign_in_at=$date AND last_sign_in_ip=$clientip where email=$email");
 	  header('Location:../index.php');
 } else {
 echo "<script type='text/javascript'>alert('Data Login Anda Salah'); window.location.href='../login/index.php';
