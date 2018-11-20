94de20451f5523c76c2d882c0d4c828b136338b2
Bintang Pratama <bp070798@gmail.com>
2018-11-20 22:39:36 +0700

    Kendaraan

--- kendaraan.php
+++ kendaraan.php
@@ -9,7 +9,7 @@
 									<th>NO</th>
 									<th>Jenis Armada</th>
 									<th>Nama Armada</th>
-									<th>Truck Qty</th>
+									<th>Max Loady</th>
 									<th>Load Type</th>
 									<th>Action</th>
 								</tr>
@@ -18,18 +18,18 @@
 								<?php 
 include 'modaladd.php';
 								$no = 1; 
-								$transid=$_SESSION['id_transporter'];
+								$transid=$_SESSION['id'];
 						
-							$sl=mysqli_query($db,"SELECT * from data_armada where transporterID='$transid'"); 
+							$sl=mysqli_query($db,"SELECT * from forwarder_vehicles where user_id='$transid'"); 
 							$s1=5;
 							while ( $dtp=mysqli_fetch_array($sl)) {
 
 							?>
 								<tr>
 									<th scope="row"><?php echo $no++ ?></th>
-									<td><?php echo $dtp['jenis_armada']; ?></td>
-									<td><?php echo $dtp['nama']; ?></td>
-									<td><?php echo $dtp['qty_truck']; ?></td>
+									<td><?php $vt=mysqli_fetch_array(mysqli_query($db,"SELECT * FROM vehicle_types where id=$dtp[vehicle_type_id]")); echo $vt[name]; ?></td>
+									<td><?php echo $dtp['name']; ?></td>
+									<td><?php echo $dtp['max_load']; ?></td>
 										<td> <?php echo $dtp['load_type']; ?>  </td>
 										<td><a href="index.php?page=tambahrute&id=<?php echo $dtp['id'];?>">Tambah Rute</a> | <a href="delete.php?id=<?php echo $dtp['id'];?>">Delete</a> </td> 
 																</tr>
